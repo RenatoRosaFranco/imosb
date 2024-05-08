67dccd8e53151f3b07ba84d1b03ad07019425012
Renato Rosa Franco <renato_ny@live.com>
2024-05-07 23:45:47 -0300

    feature: updated signup/signin screens and added ld_json loader.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def load_json_ld(file_name)
+    file_path = Rails.root.join('seo', 'ld_json', "#{file_name}.json.erb")
+
+    File.exist?(file_path) ? ld_json_file = File.read(file_path) : return
+
+    content_tag(:script, type: 'application/ld+json') do
+      ld_json_file.html_safe
+    end
+  end
 end
